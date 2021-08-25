85f426e846875f69318a38cac999d5cd115107b4
Sergey Butchak <Sergei.Butchak@gmail.com>
2021-08-26 00:02:37 +0400

    lesson 2 added request from front to back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import UserList from "./components/Users";
+import axios from "axios";
 
 class App extends React.Component {
     constructor(props) {
@@ -12,32 +13,45 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const users = [
-            {
-                "username": "django",
-                "first_name": "dsfa",
-                "last_name": "dfasdlf",
-                "email": "fjsdlfjls"
-            },
-            {
-                "username": "sergey",
-                "first_name": "dsfa",
-                "last_name": "dfasdlf",
-                "email": "fjsdlfjls"
-            },
-            {
-                "username": "test1",
-                "first_name": "dsfa",
-                "last_name": "dfasdlf",
-                "email": "fjsdlfjls"
-            }
-        ]
+        // const users = [
+        //     {
+        //         "username": "django",
+        //         "first_name": "dsfa",
+        //         "last_name": "dfasdlf",
+        //         "email": "fjsdlfjls"
+        //     },
+        //     {
+        //         "username": "sergey",
+        //         "first_name": "dsfa",
+        //         "last_name": "dfasdlf",
+        //         "email": "fjsdlfjls"
+        //     },
+        //     {
+        //         "username": "test1",
+        //         "first_name": "dsfa",
+        //         "last_name": "dfasdlf",
+        //         "email": "fjsdlfjls"
+        //     }
+        // ]
+        // this.setState(
+        //     this.state = {
+        //         "users": users
+        //     }
+        // )
 
-        this.setState(
-            this.state = {
-                "users": users
-            }
-        )
+        axios.get('http://127.0.0.1:8000/api/users')
+            .then(
+                response => {
+                    const users = response.data
+                    this.setState(
+                        {
+                            "users": users
+                        }
+                    )
+                }
+            )
+            .catch(error => console.log(error))
+
     }
 
     render() {
--- todo/settings.py
+++ todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users'
+    'corsheaders',
+    'users',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,3 +131,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.user'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
