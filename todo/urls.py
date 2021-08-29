793883f753f9ea62a881d9ca8012ac4b5990830c
Sergey.Butchak <Sergei.Butchak@gmail.com>
2021-08-29 15:17:56 +0400

    lesson 3 added todoapp in project

--- todo/urls.py
+++ todo/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
+from todoapp.views import ProjectVeiwSet, TodoViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('projects', ProjectVeiwSet)
+router.register('todo', TodoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
