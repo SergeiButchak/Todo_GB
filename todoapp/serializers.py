6903b60cb4b4542db8ce292c7a173b7f6ab6975e
Sergey.Butchak <Sergei.Butchak@gmail.com>
2021-09-03 15:59:11 +0400

    lesson 4 fixed pep8

--- todoapp/serializers.py
+++ todoapp/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from todoapp.models import Project, Todo
-from users.serializers import UserModelSerializer
 
 
 class ProjectSerializer(serializers.ModelSerializer):
