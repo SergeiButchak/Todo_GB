bb64fa0626f774faeb6f945740193f7c4e6594aa
Sergey Butchak <Sergei.Butchak@gmail.com>
2021-09-05 15:57:15 +0400

    lesson 5 added main menu

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,11 +1,29 @@
 import React from "react";
+import {HashRouter, Link} from "react-router-dom";
 
 
 const Main = () => {
     return (
 
         <div className="main">
-            TODO
+            <HashRouter>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Main</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                        <li>
+                            <Link to="/projects">Projects</Link>
+                        </li>
+                        <li>
+                            <Link to="/todo">Todos</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </HashRouter>
             <hr/>
         </div>
     )
