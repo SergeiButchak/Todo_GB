19fcc3713f2e921e304c97e0a6b1d018921c7fbf
Sergey Butchak <Sergei.Butchak@gmail.com>
2021-09-11 15:54:02 +0400

    lesson 7 added authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,18 @@ import axios from "axios";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import TodoList from "./components/Todo";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import ProjectDetail from "./components/ProjectDetail";
+import LoginForm from "./components/Auth";
+import Cookies from "universal-cookie"
+
+const NotFound404 = ({location}) => {
+    return (
+        <div>
+            <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+        </div>
+    )
+}
 
 function getUrl(endPoint) {
     const BASE_API_URL = "http://127.0.0.1:8000/api/";
@@ -21,13 +31,58 @@ class App extends React.Component {
         this.state = {
             "users": [],
             "projects": [],
-            "todos": []
+            "todos": [],
+            "login": "",
+            "token": ""
         };
     }
 
-    componentDidMount() {
+    get_token(username, password) {
+        axios.post("http://127.0.0.1:8000/api-token-auth/", {username: username, password: password})
+            .then(response => {
+                this.set_token(username, response.data['token'])
+                window.location.assign("http://localhost:3000/")
+            }).catch(error => alert("Неверный логин или пароль"))
+    }
 
-        axios.get(getUrl('users'))
+    is_authenticated() {
+        return this.state.token != ''
+    }
+
+
+    set_token(login, token) {
+        const cookies = new Cookies()
+        cookies.set("token", token)
+        cookies.set("login", login)
+        this.setState({"token": token, "login": login}, ()=>this.load_data())
+    }
+
+    logout() {
+        this.set_token('', '')
+        window.location.assign("http://localhost:3000/")
+        console.log("logout")
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get("token")
+        const login = cookies.get("login")
+        this.setState({"token": token, "login": login}, ()=>this.load_data())
+    }
+
+    get_headers() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_authenticated()) {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+
+    load_data() {
+        const headers = this.get_headers()
+        axios.get(getUrl('users'), {headers})
             .then(
                 response => {
                     const users = response.data
@@ -40,7 +95,7 @@ class App extends React.Component {
             )
             .catch(error => console.log(error))
 
-        axios.get(getUrl('projects'))
+        axios.get(getUrl('projects'), {headers})
             .then(
                 response => {
                     const projects = response.data
@@ -53,7 +108,7 @@ class App extends React.Component {
             )
             .catch(error => console.log(error))
 
-        axios.get(getUrl('todo'))
+        axios.get(getUrl('todo'), {headers})
             .then(
                 response => {
                     const todos = response.data
@@ -68,19 +123,31 @@ class App extends React.Component {
 
     }
 
+    componentDidMount() {
+        this.get_token_from_storage()
+        this.load_data()
+
+    }
+
     render() {
         return (
             // <div>Main App</div>
             <div className="App">
                 <BrowserRouter>
-                <Main />
-                    <Route exact path='/' component={() => <h1>Welcome to TODO</h1>} />
-                    <Route exact path='/users' component={() => <UserList users={this.state.users}/>} />
-                    <Route exact path='/projects' component={() =>  <ProjectList projects={this.state.projects}/>} />
-                    <Route exact path='/projects/:id' component={() =>  <ProjectDetail projects={this.state.projects}/>} />
-                    <Route exact path='/todo' component={() => <TodoList todos={this.state.todos} />} />
+                    <Main instance={this}/>
+                    <Switch>
+                        <Route exact path='/' component={() => <h1>Welcome to TODO</h1>}/>
+                        <Route exact path='/users' component={() => <UserList users={this.state.users}/>}/>
+                        <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects}/>}/>
+                        <Route exact path='/projects/:id'
+                               component={() => <ProjectDetail projects={this.state.projects}/>}/>
+                        <Route exact path='/todo' component={() => <TodoList todos={this.state.todos}/>}/>
+                        <Route exact path='/login' component={() => <LoginForm
+                            get_token={(username, password) => this.get_token(username, password)}/>}/>
+                        <Route component={NotFound404}/>
+                    </Switch>
                 </BrowserRouter>
-                <Footer />
+                <Footer/>
             </div>
         )
     }
